951fc0decd6d85a1ebec7c46310a9bba0ac60fa3
trinityXmontoya <trinityXmontoya@gmail.com>
2019-05-24 18:26:32 -0400

    formatting fix

--- sorting-algorithms/merge-sort-in-clojure.clj
+++ sorting-algorithms/merge-sort-in-clojure.clj
@@ -5,14 +5,14 @@
   (if (< (count coll) 2)
     coll
     (let [[left right] (split-at (/ (count coll) 2) coll)]
-  		  (loop [final-coll []
-          			left (dat-merge-sort left)
-         				right (dat-merge-sort right)]
-      		(cond
-        		(empty? left) 																			 (concat final-coll right)
-        		(empty? right) 																			(concat final-coll left)
-        		(< (first left) (first right)) 			(recur (conj final-coll (first left)) (rest left) right)
-        		:else 																												(recur (conj final-coll (first right)) left (rest right)))))))
+      (loop [final-coll []
+             left (dat-merge-sort left)
+             right (dat-merge-sort right)]
+        (cond
+          (empty? left)									 (concat final-coll right)
+          (empty? right) 								 (concat final-coll left)
+          (< (first left) (first right)) (recur (conj final-coll (first left)) (rest left) right)
+          :else													 (recur (conj final-coll (first right)) left (rest right)))))))
 
 ; sample coll
 (def coll-size 999)
