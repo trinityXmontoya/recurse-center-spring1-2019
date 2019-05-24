3bc528313ae717c4815aad57b311fbbfacb00ac4
trinityXmontoya <trinityXmontoya@gmail.com>
2019-05-24 18:18:27 -0400

    make it more clojure-y and refactor

--- sorting-algorithms/merge-sort-in-clojure.clj
+++ sorting-algorithms/merge-sort-in-clojure.clj
@@ -1,36 +1,30 @@
-(declare merge-two-sorted-arrs-into-one-sorted-arr)
-
-(defn nearest-whole-number
-  [i]
-  (int (Math/ceil i)))
+(declare merge-two-sorted-colls-into-one-sorted-coll)
 
 (defn dat-merge-sort
-  [arr]
-  (if (< (count arr) 2)
-    arr
-    (let [mid-index (nearest-whole-number (/ (count arr) 2))
-          [left right] (partition mid-index mid-index nil arr)]
-      (merge-two-sorted-arrs-into-one-sorted-arr (dat-merge-sort left) (dat-merge-sort right)))))
+  [coll]
+  (if (< (count coll) 2)
+    coll
+    (let [[left right] (split-at (/ (count coll) 2) coll)]
+      (merge-two-sorted-colls-into-one-sorted-coll (dat-merge-sort left) (dat-merge-sort right)))))
 
-(defn merge-two-sorted-arrs-into-one-sorted-arr
-  [arr1 arr2]
-  (loop [final-arr []
-         arr1 arr1
-         arr2 arr2]
-      (cond 
-        (and (empty? arr1) (empty? arr2)) final-arr
-        (empty? arr1) 																			 (recur (concat final-arr arr2) nil nil)
-        (empty? arr2) 																				(recur (concat final-arr arr1) nil nil)
-        (< (first arr1) (first arr2)) 				(recur (conj final-arr (first arr1)) (drop 1 arr1) arr2)
-        :else 																												(recur (conj final-arr (first arr2)) arr1 (drop 1 arr2)))))
+(defn merge-two-sorted-colls-into-one-sorted-coll
+  [left right]
+  (loop [final-coll []
+         left left
+         right right]
+      (cond
+        (empty? left) 																			 (concat final-coll right)
+        (empty? right) 																			(concat final-coll left)
+        (< (first left) (first right)) 			(recur (conj final-coll (first left)) (rest left) right)
+        :else 																												(recur (conj final-coll (first right)) left (rest right)))))
 
-; sample arr
-(def arr-size 999)
-(def sample-arr (repeatedly arr-size #(rand-int 100000)))
+; sample coll
+(def coll-size 999)
+(def sample-coll (repeatedly coll-size #(rand-int 100000)))
 
 ; does it work?
-(println (str "dat merge sort worked? " (= (dat-merge-sort sample-arr) (sort sample-arr))))
+(println (str "dat merge sort worked? " (= (dat-merge-sort sample-coll) (sort sample-coll))))
 
-; how long doees it take?
-(println (str "for array of size " arr-size))
-(time (dat-merge-sort sample-arr))
+; how long does it take?
+(println (str "for coll of size " coll-size))
+(time (dat-merge-sort sample-coll))
